073278e433a8123fa916d8470db6c3e534b5ced4
Talitta Nunes <talitta-sossego@github.com>
2023-08-20 17:33:48 -0300

    improving the app.py file

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-
 from flask import Flask, render_template, request, jsonify
 import requests
 
-app = Flask(__name__) #recomendação do flask. sempre.
+app = Flask(__name__)
 
-#sempre tem um route e uma função. Routo o caminho que o site terá, homepage.
-# função, o que eu desejo exibir nessa página.
 def get_address_from_cep(cep):
     url = f"https://viacep.com.br/ws/{cep}/json/"
     response = requests.get(url)
@@ -26,20 +23,6 @@ def get_address_from_cep(cep):
     else:
         return None
 
-# Example usage
-cep = input("Enter the CEP: ")
-address_info = get_address_from_cep(cep)
-
-if address_info:
-    print("Address:")
-    print(f"CEP: {address_info['cep']}")
-    print(f"Logradouro: {address_info['logradouro']}")
-    print(f"Complemento: {address_info['complemento']}")
-    print(f"Bairro: {address_info['bairro']}")
-    print(f"Localidade: {address_info['localidade']}")
-    print(f"UF: {address_info['uf']}")
-else:
-    print("Address not found for the provided CEP.")
 
 @app.route('/')
 def index():
@@ -50,8 +33,6 @@ def get_address():
     cep = request.form['cep']
     address_info = get_address_from_cep(cep)
     return jsonify(address_info)
-##irei exexutar esse codigo se esse arquivo for executado, colocando o site no ar
 
 if __name__ == '__main__':
     app.run(debug=True)
-
