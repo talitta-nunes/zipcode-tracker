f1a220aeabf793dae5d5a5eaac140ef7ede411c3
Talitta Nunes <talitta-sossego@github.com>
2023-12-12 10:33:19 -0300

    gitignore

--- app.py
+++ app.py
@@ -1,28 +1,7 @@
 from flask import Flask, render_template, request, jsonify
-import requests
+from utils import get_address_from_cep
 
 app = Flask(__name__)
-
-def get_address_from_cep(cep):
-    url = f"https://viacep.com.br/ws/{cep}/json/"
-    response = requests.get(url)
-    
-    if response.status_code == 200:
-        data = response.json()
-        if not data.get("erro"):
-            return {
-                "cep": data["cep"],
-                "logradouro": data["logradouro"],
-                "complemento": data.get("complemento", ""),
-                "bairro": data["bairro"],
-                "localidade": data["localidade"],
-                "uf": data["uf"]
-            }
-        else:
-            return None
-    else:
-        return None
-
 
 @app.route('/')
 def index():
@@ -32,7 +11,12 @@ def index():
 def get_address():
     cep = request.form['cep']
     address_info = get_address_from_cep(cep)
-    return jsonify(address_info)
+    return jsonify(address_info), cep
+# @app.route('/get_coordinate')
+# def get_coord():
+#     coord = get_address['cep']
+#     coord_info = get_coordinate(coord)
+#     return jsonify(coord_info)
 
 if __name__ == '__main__':
     app.run(debug=True)
