ce7238f40e2e97d4fb023c16afd3d5a8634d6107
Talitta Nunes <talitta-sossego@github.com>
2023-12-12 10:42:21 -0300

    fixing the cep function

--- app.py
+++ app.py
@@ -11,12 +11,8 @@ def index():
 def get_address():
     cep = request.form['cep']
     address_info = get_address_from_cep(cep)
-    return jsonify(address_info), cep
-# @app.route('/get_coordinate')
-# def get_coord():
-#     coord = get_address['cep']
-#     coord_info = get_coordinate(coord)
-#     return jsonify(coord_info)
+    return jsonify(address_info)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
